62c54f5ba1b0edb27405953cc85a4bb8e41e49ce
Hchyeria <hhh@hchyeria.com>
2020-02-14 23:04:37 +0800

    add record

--- themes/avalon/source/js/function/highlight/load.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded',function() {
-    document.getElementById("load-process").style.width = '100%'
-    $("#loading").remove()
-    $(".remove-item").remove()
-})
